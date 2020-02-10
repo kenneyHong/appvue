99308dacb937bd48cf638dd072000fdb3d9f6e27
floodKay <59719206+floodKay@users.noreply.github.com>
2020-02-11 01:11:00 +0800

    货品入库表格

--- src/views/cargoStorage/index.vue
+++ src/views/cargoStorage/index.vue
@@ -45,41 +45,111 @@
     </div> 
     <div class="middleBorder">
       <el-form ref="form" :inline="true" class="demo-form-inline" :model="form" label-width="120px">
-        <el-form-item label="单据编号：">
-          <el-input v-model="form.documentNumber"></el-input>
-        </el-form-item>
-        <el-form-item label="供应商：">
-          <el-input v-model="form.supplier"></el-input>
-        </el-form-item>
-        <el-form-item label="创建时间：">
-          <el-date-picker
-            v-model="form.creationTime"
-            type="date"
-            placeholder="2016/01/01-2016/02/01">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="送货单号：">
-          <el-input v-model="form.singleNumber"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary">搜索</el-button>
-           <el-button>重置</el-button>
-        </el-form-item>
-        <el-form-item label="采购员：">
-          <el-input v-model="form.buyer"></el-input>
-        </el-form-item>
-        <el-form-item label="最后操作时间：">
-          <el-date-picker
-            v-model="form.lastMinute"
-            type="date"
-            placeholder="2016/01/01-2016/02/01">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="备注：">
-          <el-input v-model="form.note"></el-input>
-        </el-form-item>
-      </el-form> 
-    </div> 
+        <div class="leftlayout">
+          <el-form-item label="单据编号：">
+            <el-input v-model="form.documentNumber"></el-input>
+          </el-form-item>
+          <el-form-item label="供应商：">
+            <el-input v-model="form.supplier"></el-input>
+          </el-form-item>
+          <el-form-item label="创建时间：">
+            <el-date-picker
+              v-model="form.creationTime"
+              type="date"
+              placeholder="2016/01/01-2016/02/01">
+            </el-date-picker>
+          </el-form-item>
+          <el-form-item label="送货单号：">
+            <el-input v-model="form.singleNumber"></el-input>
+          </el-form-item>
+          <el-form-item label="采购员：">
+            <el-input v-model="form.buyer"></el-input>
+          </el-form-item>
+          <el-form-item label="最后操作时间：">
+            <el-date-picker
+              v-model="form.lastMinute"
+              type="date"
+              placeholder="2016/01/01-2016/02/01">
+            </el-date-picker>
+          </el-form-item>
+          <el-form-item label="备注：">
+            <el-input v-model="form.note"></el-input>  
+          </el-form-item>
+        </div>
+        <div class="rightLayout">
+          <el-form-item>
+            <el-button type="primary">搜索</el-button>
+            <el-button>重置</el-button>
+          </el-form-item>
+        </div>  
+      </el-form>
+    </div>
+    <el-table
+      :data="tableData">
+      <el-table-column
+        type="selection"
+        width="55">
+      </el-table-column>
+      <el-table-column
+        prop="documentNumber"
+        label="单号编号"
+        sortable
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="supplier"
+        label="供应商"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="source"
+        label="来源"
+        width='120'>
+      </el-table-column>
+      <el-table-column
+        prop="creationTime"
+        sortable
+        label="创建时间"
+        width='180'>
+      </el-table-column>
+      <el-table-column
+        prop="buyer"
+        label="采购员"
+        width='120'>
+      </el-table-column>
+      <el-table-column
+        prop="quantity"
+        label="采购数量">
+      </el-table-column>
+      <el-table-column
+        prop="cost"
+        label="采购成本">
+      </el-table-column>
+      <el-table-column
+        prop="lastMinute"
+        sortable
+        label="最后操作时间"
+        width='180'>
+      </el-table-column>
+      <el-table-column
+        prop="status"
+        label="状态">
+      </el-table-column>
+        <el-table-column
+        prop="operating"
+        label="操作"> 
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="block">
+     <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="100">
+      </el-pagination>
+    </div>
   </div>
 </template>
 <script>
@@ -92,11 +162,56 @@ export default {
         documentNumber: '',
         supplier: '',
         creationTime: '',
-        singleNumber:'',
-        buyer:'',
-        lastMinute:'',
-        note:''
-      }
+        singleNumber: '',
+        buyer: '',
+        lastMinute: '',
+        note: ''
+      },
+      tableData: [{
+        documentNumber: '17000001',
+        supplier: 'xxxxx公司',
+        source: '自建',
+        creationTime: '',
+        buyer: '张三',
+        quantity: '100',
+        cost: '10000.00',
+        lastMinute: '',
+        status: '草稿',
+        operating: '查看'
+      }, {
+        documentNumber: '17000001',
+        supplier: '总部',
+        source: '总部分发',
+        creationTime: '',
+        buyer: '',
+        quantity: '',
+        cost: '',
+        lastMinute: '',
+        status: '待审查',
+        operating: '查看'
+      }, {
+        documentNumber: '17000001',
+        supplier: '总部',
+        source: '总部分发',
+        creationTime: '',
+        buyer: '',
+        quantity: '',
+        cost: '',
+        lastMinute: '',
+        status: '待审查',
+        operating: '查看'
+      }, {
+        documentNumber: '17000001',
+        supplier: '总部',
+        source: '总部分发',
+        creationTime: '',
+        buyer: '',
+        quantity: '',
+        cost: '',
+        lastMinute: '',
+        status: '待审查',
+        operating: '查看'
+      }]
     }
   },
   methods: {
@@ -155,7 +270,19 @@ export default {
      margin-left: 10px;
      margin-top: 10px;
    }
-   
+   .leftlayout{
+     float: left;
+     width: 70%;
+   }
+   .rightLayout{
+     float: right;
+     width: 30%;
+   }
   }
+  .el-table{
+    border-top: 1px solid #e5e5e5;
+    margin-top: 10px;
+  }
+  
 }
 </style>
