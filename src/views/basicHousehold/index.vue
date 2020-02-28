972483e3839559f09f161ca125c3d6ecb166a255
floodKay <59719206+floodKay@users.noreply.github.com>
2020-02-28 15:07:14 +0800

    电子钱包(基本户)流水

--- src/views/basicHousehold/index.vue
+++ src/views/basicHousehold/index.vue
@@ -18,94 +18,102 @@
       </el-col>
     </el-row>
     <div class="middleBorder">
-      <el-form ref="form" :inline="true" class="demo-form-inline" :model="form" label-width="118px">
-        <el-row :gutter="20">
-          <el-col :span="18">
-            <el-form-item label="时间：" label-width="120px">
-              <el-date-picker
-                v-model="form.time"
-                type="daterange"
-                range-separator="-"
-                start-placeholder="2016/01/01"
-                end-placeholder="2016/01/01">
-              </el-date-picker>
-            </el-form-item>
-            <el-form-item label="资金账号：">
-              <el-input v-model="form.fundAccount"></el-input>
-            </el-form-item>
-            <el-form-item label="户名：">
-              <el-input v-model="form.accountName"></el-input>
-            </el-form-item>
-            <el-form-item label="商户类型：">
-              <el-select v-model="formInline.region" placeholder="全部">
-                <el-option label="全部" value="all"></el-option>
-                <el-option label="珠宝公司" value="jewelryCompany"></el-option>
-                <el-option label="珠宝门店" value="jewelryStore"></el-option>
-                <el-option label="礼品供应商" value="giftSupplier"></el-option>
-                <el-option label="平台" value="gift"></el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item label="公司编码：">
-              <el-input v-model="form.companyCode"></el-input>
-            </el-form-item>
-            <el-form-item label="公司名称：">
-              <el-input v-model="form.companyName"></el-input>
-            </el-form-item>
-            </el-form-item>
-              <el-form-item label="门店编码：">
-              <el-input v-model="form.storeCode"></el-input>
-            </el-form-item>
-            </el-form-item>
-              <el-form-item label="门店名称：">
-              <el-input v-model="form.storeName"></el-input>
-            </el-form-item> 
-            <el-form-item label="来源：">
-              <el-select v-model="formInline.region1" placeholder="全部">
-                <el-option label="全部" value="all"></el-option>
-                <el-option label="提现" value="withdraw"></el-option>
-                <el-option label="零售单" value="retailOrder"></el-option>
-                <el-option label="充值" value="recharge"></el-option>
-                <el-option label="SaaS订单" value="SaaSOrders"></el-option>
-                <el-option label="短信充值" value="SMSTopup"></el-option>
-                <el-option label="科技院订单" value="academyOrder"></el-option>
-                <el-option label="定制单" value="customOrder"></el-option>
-                <el-option label="转账" value="transfer"></el-option>
-                <el-option label="零售退货单" value="retailReturn"></el-option>
-              </el-select>
-            </el-form-item>
-            <el-row>
-              <el-col :span="6"><div class="grid-content bg-purple">
-                <span class="number">￥300,000.00</span>
-                <span class="accountBalance">期初账户余额</span>
-                </div>
-              </el-col>
-              <el-col :span="6"><div class="grid-content bg-purple-light">
-                <span class="number">￥99,000.00</span>
-                <span class="currentIncome">本期收入</span>
-                <span class="transactionAmount">交易金额￥100,000.00，服务费￥1,000.00</span>
-                </div>
-              </el-col>
-              <el-col :span="6"><div class="grid-content bg-purple">
-                <span class="number">￥201,000.00</span>
-                <span class="currentExpenditure">本期支出</span>
-                <span class="transactionAmount">交易金额￥200,000.00，服务费￥1,000.00</span>
-                </div>
-              </el-col>
-              <el-col :span="6"><div class="grid-content bg-purple-light">
-                <span class="number">￥￥198,000.00</span>
-                <span class="endingAccountbalance">期末账户余额</span>
-                </div>
-              </el-col>
-          </el-row> 
-          </el-col>
-          <el-col :span="6">
-            <el-form-item>
-              <el-button type="primary">搜索</el-button>
-              <el-button>重置</el-button>
-              <el-button>导出</el-button>
-            </el-form-item>
-          </el-col>
-        </el-row>    
+      <el-form ref="form" :inline="true" class="demo-form-inline" :model="form" label-width="120px">
+        <div class="Upperlayout">
+          <el-row :gutter="20">
+            <el-col :span="18">
+              <el-form-item label="时间：" label-width="120px">
+                <el-date-picker
+                  v-model="form.time"
+                  type="daterange"
+                  range-separator="-"
+                  start-placeholder="2016/01/01"
+                  end-placeholder="2016/01/01">
+                </el-date-picker>
+              </el-form-item>
+              <el-form-item label="资金账号：">
+                <el-input v-model="form.fundAccount"></el-input>
+              </el-form-item>
+              <el-form-item label="户名：">
+                <el-input v-model="form.accountName"></el-input>
+              </el-form-item>
+              <el-form-item label="商户类型：">
+                <el-select v-model="formInline.region" placeholder="全部">
+                  <el-option label="全部" value="all"></el-option>
+                  <el-option label="珠宝公司" value="jewelryCompany"></el-option>
+                  <el-option label="珠宝门店" value="jewelryStore"></el-option>
+                  <el-option label="礼品供应商" value="giftSupplier"></el-option>
+                  <el-option label="平台" value="gift"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="公司编码：">
+                <el-input v-model="form.companyCode"></el-input>
+              </el-form-item>
+              <el-form-item label="公司名称：">
+                <el-input v-model="form.companyName"></el-input>
+              </el-form-item>
+              </el-form-item>
+                <el-form-item label="门店编码：">
+                <el-input v-model="form.storeCode"></el-input>
+              </el-form-item>
+              </el-form-item>
+                <el-form-item label="门店名称：">
+                <el-input v-model="form.storeName"></el-input>
+              </el-form-item> 
+              <el-form-item label="来源：">
+                <el-select v-model="formInline.region1" placeholder="全部">
+                  <el-option label="全部" value="all"></el-option>
+                  <el-option label="提现" value="withdraw"></el-option>
+                  <el-option label="零售单" value="retailOrder"></el-option>
+                  <el-option label="充值" value="recharge"></el-option>
+                  <el-option label="SaaS订单" value="SaaSOrders"></el-option>
+                  <el-option label="短信充值" value="SMSTopup"></el-option>
+                  <el-option label="科技院订单" value="academyOrder"></el-option>
+                  <el-option label="定制单" value="customOrder"></el-option>
+                  <el-option label="转账" value="transfer"></el-option>
+                  <el-option label="零售退货单" value="retailReturn"></el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="6">
+              <el-form-item>
+                <el-button type="primary">搜索</el-button>
+                <el-button>重置</el-button>
+                <el-button>导出</el-button>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </div>
+        <div class="lowerLayout">
+          <el-row :gutter="25">
+            <el-col :span="6">
+              <div class="grid-content bg-purple">
+                <div class="number1">￥300,000.00</div>
+                <div class="accountBalance">期初账户余额</div>
+              </div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content bg-purple-light">
+                <div class="number2">￥99,000.00</div>
+                <div class="currentIncome">本期收入</div>
+                <div class="transactionAmount">交易金额￥100,000.00，服务费￥1,000.00</div>
+              </div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content bg-purple">
+                <div class="number3">￥201,000.00</div>
+                <div class="currentExpenditure">本期支出</div>
+                <div class="transactionAmount1">交易金额￥200,000.00，服务费￥1,000.00</div>
+              </div>
+            </el-col>
+            <el-col :span="6">
+              <div class="grid-content bg-purple-light">
+                <div class="number4">￥￥198,000.00</div>
+                <div class="endingAccountbalance">期末账户余额</div>
+              </div>
+            </el-col>
+            </el-row> 
+        </div>      
      </el-form>       
     </div>
     <el-table
@@ -395,8 +403,10 @@ background-color: #f9fafc;
     background: #e5e9f2;
   }
   .grid-content {
-    min-height: 36px;
-    margin-left: 10px;
+    min-height: 56px;
+    text-align: center;
+    width: 230px;
+    padding-top: 5px;
   }
   .row-bg {
     padding: 10px 0;
